1ec09327163d67059a5b4db52e2979548f369f1e
honghongquan <quanhoangkk01@gmail.com>
2020-11-22 09:21:13 +0700

    Enemy
    update

---
+++ Assets/Scripts/Enemy/HealthScrip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeathScrip : MonoBehaviour
+{
+    public float startHealth;
+    public float hp;
+    void Start()
+    {
+        hp = startHealth; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+
+        if(hp<= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+       // if(die)
+    }
+}
