fb68ae113466817afa35bfbb6057accfc561d59a
honghongquan <quanhoangkk01@gmail.com>
2020-11-25 20:27:27 +0700

    BossBig
    New

---
+++ Assets/Scripts/Enemy/HotZoneCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotZoneCheck : MonoBehaviour
+{
+    private Enemy_behaviour enemyParent;
+    private bool inRange;
+    private Animator anim;
+    private void Awake()
+    {
+        enemyParent = GetComponent<Enemy_behaviour>();
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(inRange && !anim.GetCurrentAnimatorStateInfo(0).IsName("Boss_attack"))
+        {
+            enemyParent.Flip();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
+            gameObject.SetActive(false);
+            enemyParent.triggerArea.SetActive(true);
+            enemyParent.inRange = true;
+            enemyParent.SelectTarget();
+        }
+    }
+}
